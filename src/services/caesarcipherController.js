b68b5859aa295710c772ade638e7a3cb58abb98e
Karolinny Guimarães <karolinny.silva@gmail.com>
2020-05-03 22:41:04 -0300

    refactored

--- src/services/caesarcipherController.js
+++ src/services/caesarcipherController.js
@@ -7,13 +7,12 @@ exports.getEncrypted = async (req, res) => {
 	const response = await axios.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=d2be53b4cb4591f19ca1bf97dfc57a94b6079aee');
 	answer = response.data;
 	answer.decifrado = decipherer(answer);
+	answer.cifrado = answer.cifrado.toLowerCase();
 	answer.resumo_criptografico = sha1(answer.decifrado);
 	fs.writeFileSync('answer.json', JSON.stringify(answer));
 
 	const formData = new FormData();
-	const answer_file = fs.createReadStream('answer.json');
-
-	formData.append('answer', answer_file);
+	formData.append('answer', fs.createReadStream('answer.json'));
 	
 	const submit_response = await axios.post('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=d2be53b4cb4591f19ca1bf97dfc57a94b6079aee', formData, {
 		headers: formData.getHeaders(),
@@ -22,24 +21,13 @@ exports.getEncrypted = async (req, res) => {
 	res.send(submit_response.data);
 };
 
-function decipherer(answer) {
-	const alphabet_array = new Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 97 + i ) );
+const decipherer = (answer) => {
+	const alphabet = new Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 97 + i ) );
 	let decifrado = '';
 
-	const alphabet = new Proxy(alphabet_array, {
-    get(target, prop) {
-			if (!isNaN(prop)) {
-				prop = parseInt(prop, alphabet_array.lengt);
-				if (prop < 0) {
-					prop += target.length;
-				}
-			}
-      return target[prop];
-    }
-	});
-
 	for (let letter of answer.cifrado){
-		decifrado += alphabet_array.indexOf(letter) < 0 ? letter : alphabet[ (alphabet_array.indexOf(letter) - answer.numero_casas) ]
+		let index = (alphabet.indexOf(letter) - answer.numero_casas);
+		decifrado += alphabet.indexOf(letter) < 0 ? letter : alphabet[ index >= 0 ? index : (alphabet.length + index) ]
 	}
 
 	return decifrado;
